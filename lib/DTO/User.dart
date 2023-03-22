fce1f0ed895f54786c2ecd912f315a9377866270
ManuelAnony <manuelfelipelancheros@gmail.com>
2023-03-22 01:43:58 -0500

    Commit dos prueba de git hub

---
+++ lib/DTO/User.dart
@@ -0,0 +1,6 @@
+class User {
+    String name;
+    String address;
+
+    User({required this.name, required this.address});
+}
--- lib/main_comprador.dart
+++ lib/main_comprador.dart
@@ -14,7 +14,6 @@ void main() async{
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var myItemsCollection;
     return MaterialApp(
       title: 'My App',
       home: Compradores() // pantalla de inicio nueva
