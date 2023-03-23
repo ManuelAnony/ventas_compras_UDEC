03796fd9b81f00f4aea77e557f4efa4ee3fb6be7
ManuelAnony <manuelfelipelancheros@gmail.com>
2023-03-22 23:05:43 -0500

    Configuracion de textfile para usuario y direccion y booton, faltando la funcion del boton

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My App',
-      home: Inventario(), // pantalla de inicio nueva
+    return MaterialApp( debugShowCheckedModeBanner: false , home: InicioVendedor());
+  }
+}
+class InicioVendedor extends StatefulWidget {
+  @override
+  HomeVendedor createState() => HomeVendedor();
+}
+
+class HomeVendedor extends State<InicioVendedor> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mi Aplicación'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Botón de interruptor (Switch)
+            Switch(
+              value: true,
+              onChanged: (value) {},
+            ),
+
+            // Botón para ir a inventarios
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Inventario()));
+
+              },
+              child: const Text('Ir a Inventarios'),
+            ),
+
+            // Botón para ir a la base de pedidos
+            ElevatedButton(
+              onPressed: () {
+
+              },
+              child: const Text('Ir a Base de Pedidos'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
